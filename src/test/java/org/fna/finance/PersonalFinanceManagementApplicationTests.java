64bf07f98c45dd3d5bb1ac9c5d0d8b883ce47a82
Narcis Fanica <narcis.fanica@vivacredit.ro>
2024-12-07 13:03:52 +0200

    Init

---
+++ src/test/java/org/fna/finance/PersonalFinanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fna.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
